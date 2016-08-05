127f16c796793333f11d4d8c52db78c6404f2675
Herman Barrantes <barrantesgerman@gmail.com>
2016-08-05 15:26:34 -0600

    Added Pure CSS

--- src/main/java/org/habv/mnemosyne/controller/IndexController.java
+++ src/main/java/org/habv/mnemosyne/controller/IndexController.java
@@ -1,5 +1,7 @@
 package org.habv.mnemosyne.controller;
 
+import org.habv.mnemosyne.repository.EntryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,10 +12,13 @@ import org.springframework.web.bind.annotation.GetMapping;
  */
 @Controller
 public class IndexController {
+    
+    @Autowired
+    private EntryRepository entryRepository;
 
     @GetMapping(value = {"/", "/index"})
     public String index(Model model) {
-        model.addAttribute("mensaje", "Hola Mundo");
+        model.addAttribute("entries", entryRepository.findAll());
         return "index";
     }
 }
