9c32df4dc6c8178dcd10114db51bcd7554491a2b
Herman Barrantes <barrantesgerman@gmail.com>
2016-08-29 19:56:33 -0600

    Manage Session with MongoDB

--- src/main/java/org/habv/mnemosyne/SpringSessionConfig.java
+++ src/main/java/org/habv/mnemosyne/SpringSessionConfig.java
@@ -1,13 +1,20 @@
 package org.habv.mnemosyne;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.session.data.mongo.JdkMongoSessionConverter;
+import org.springframework.session.data.mongo.config.annotation.web.http.EnableMongoHttpSession;
 
 /**
  * @author Herman Barrantes
  * @since 11/08/2016
  */
 @Configuration
-@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 10)
+@EnableMongoHttpSession(maxInactiveIntervalInSeconds = 300)
 public class SpringSessionConfig {
+
+    @Bean
+    public JdkMongoSessionConverter jdkMongoSessionConverter() {
+        return new JdkMongoSessionConverter();
+    }
 }
