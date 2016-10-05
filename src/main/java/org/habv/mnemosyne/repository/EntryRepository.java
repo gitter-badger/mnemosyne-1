a402a4601b1c2ca5d4bc68c8ffa7effec4b74420
Herman Barrantes <barrantesgerman@gmail.com>
2016-10-04 18:33:36 -0600

    Remove spring data rest

--- src/main/java/org/habv/mnemosyne/repository/EntryRepository.java
+++ src/main/java/org/habv/mnemosyne/repository/EntryRepository.java
@@ -4,23 +4,16 @@ import org.habv.mnemosyne.model.Entry;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.data.rest.core.annotation.RestResource;
 
 /**
  * @author Herman Barrantes
  * @since 04/08/2016
  */
-@RepositoryRestResource(collectionResourceRel = "entries", path = "entries")
 public interface EntryRepository extends MongoRepository<Entry, String>, EntryRepositoryCustom {
 
-    @RestResource(path = "entries", rel = "entries")
     Page<Entry> findByOrderByDateDesc(Pageable pageable);
 
-    @RestResource(path = "entriesByCategory", rel = "entriesByCategory")
-    Page<Entry> findByCategoryOrderByDateDesc(@Param("category") String category, Pageable pageable);
+    Page<Entry> findByCategoryOrderByDateDesc(String category, Pageable pageable);
 
-    @RestResource(path = "entriesByTag", rel = "entriesByTag")
-    Page<Entry> findByTagsOrderByDateDesc(@Param("tag") String tag, Pageable pageable);
+    Page<Entry> findByTagsOrderByDateDesc(String tag, Pageable pageable);
 }
